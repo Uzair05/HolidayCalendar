cbbef80219f62d9536fc9d4b676185c1287b73e7
Uzair Bin Colangoy Asim <u3560307@connect.hku.hk>
2020-09-12 10:55:11 +0500

    rewind

--- assets/js/weatherStrap.js
+++ assets/js/weatherStrap.js
@@ -5,11 +5,14 @@ window.onload = ()=>{
     var box = document.getElementById("weatherBOX");
     
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?appid=c5c81d1d2b51807c753d34b4ab9227db&units=metric&lang=en&q=" + "Lahore"/*cityName*/)
+    fetch("https://api.openweathermap.org/data/2.5/forecast?appid=c5c81d1d2b51807c753d34b4ab9227db&units=metric&lang=en&q=" + "Hong%20Kong"/*cityName*/)
     .then(response => response.json())
     .then((data)=>{
         if (data.cod = "200"){
-            console.table(data.city)
+            data = data.list;
+            
+            console.table(data)
+            
         }
     })
 
